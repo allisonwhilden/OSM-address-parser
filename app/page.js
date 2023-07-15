d86261581908f11f2fb8a2457964c4624d6fefbe
allisonwhilden <allisonwhilden@gmail.com>
2023-07-15 16:40:30 -0700

    Add copy to clipboard and unit parsing

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import {
   Textarea,
   useInput,
@@ -7,6 +7,7 @@ import {
   Spacer,
   createTheme,
   NextUIProvider,
+  Container,
 } from "@nextui-org/react";
 import { Copy } from "iconoir-react";
 var parser = require("parse-address");
@@ -29,13 +30,21 @@ export default function Home() {
   );
   var parsed = parser.parseLocation(address);
 
-  // Controlled
+  // Controlled for input
   const {
     value: controlledValue,
     setValue: setControlledValue,
     reset,
     bindings,
   } = useInput("15946 Redmond Way, Suite 103 Redmond, WA 98052");
+
+  // Ref for output
+  const outputTextAreaRef = useRef(null);
+  const onPress = () => {
+    if (outputTextAreaRef.current) {
+      navigator.clipboard.writeText(outputTextAreaRef.current.value);
+    }
+  };
 
   return (
     <NextThemesProvider
@@ -47,41 +56,54 @@ export default function Home() {
       }}
     >
       <NextUIProvider>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-            height: "100vh",
-          }}
-        >
-          <Textarea
-            {...bindings}
-            width={500}
-            label="Address to parse"
-            minRows={2}
-          />
-          <Spacer y={1} />
-          <Textarea
-            readOnly
-            label="Output"
-            width={500}
-            minRows={6}
-            value={`addr:housenumber  ${
-              parser.parseLocation(bindings.value).number
-            }
+        <Container>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              height: "100vh",
+              width: "100%",
+            }}
+          >
+            <Textarea
+              {...bindings}
+              width={"100%"}
+              label="Address to parse"
+              minRows={2}
+            />
+            <Spacer y={1} />
+            <Textarea
+              readOnly
+              bordered
+              ref={outputTextAreaRef}
+              label="Output"
+              width={"100%"}
+              minRows={6}
+              value={`addr:housenumber  ${
+                parser.parseLocation(bindings.value).number
+              }
 addr:street       ${parser.parseLocation(bindings.value).street}
-addr:unit	  ???
+addr:unit        ${parser.parseLocation(bindings.value).sec_unit_num} 
 addr:city         ${parser.parseLocation(bindings.value).city}
 addr:state        ${parser.parseLocation(bindings.value).state}
 addr:postcode     ${parser.parseLocation(bindings.value).zip}
         `}
-          />
+            />
 
-          <Spacer y={1} />
-          <Button iconRight={<Copy outline="currentColor" />}></Button>
-        </div>
+            <Spacer y={1} />
+            <Button
+              rounded
+              color="secondary"
+              flat
+              icon={<Copy outline="currentColor" />}
+              onPress={onPress}
+            >
+              Copy
+            </Button>
+          </div>
+        </Container>
       </NextUIProvider>
     </NextThemesProvider>
   );
