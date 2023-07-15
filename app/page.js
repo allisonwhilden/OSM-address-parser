2d55582bf6ecb6555ed448e200a791bef3dfef44
allisonwhilden <allisonwhilden@gmail.com>
2023-07-15 16:28:00 -0700

    Add dark mode

--- app/page.js
+++ app/page.js
@@ -1,8 +1,27 @@
 "use client";
 import { useState } from "react";
-import { Textarea, useInput, Button, Spacer } from "@nextui-org/react";
+import {
+  Textarea,
+  useInput,
+  Button,
+  Spacer,
+  createTheme,
+  NextUIProvider,
+} from "@nextui-org/react";
 import { Copy } from "iconoir-react";
 var parser = require("parse-address");
+import {
+  ThemeProvider as NextThemesProvider,
+  useTheme as useNextTheme,
+} from "next-themes";
+
+const lightTheme = createTheme({
+  type: "light",
+});
+
+const darkTheme = createTheme({
+  type: "dark",
+});
 
 export default function Home() {
   const [address, setAddress] = useState(
@@ -19,38 +38,51 @@ export default function Home() {
   } = useInput("15946 Redmond Way, Suite 103 Redmond, WA 98052");
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        height: "100vh",
+    <NextThemesProvider
+      defaultTheme="system"
+      attribute="class"
+      value={{
+        light: lightTheme.className,
+        dark: darkTheme.className,
       }}
     >
-      <Textarea
-        {...bindings}
-        width={500}
-        label="Address to parse"
-        minRows={2}
-      />
-      <Spacer y={1} />
-      <Textarea
-        readOnly
-        label="Output"
-        width={500}
-        minRows={6}
-        value={`addr:housenumber  ${parser.parseLocation(bindings.value).number}
+      <NextUIProvider>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100vh",
+          }}
+        >
+          <Textarea
+            {...bindings}
+            width={500}
+            label="Address to parse"
+            minRows={2}
+          />
+          <Spacer y={1} />
+          <Textarea
+            readOnly
+            label="Output"
+            width={500}
+            minRows={6}
+            value={`addr:housenumber  ${
+              parser.parseLocation(bindings.value).number
+            }
 addr:street       ${parser.parseLocation(bindings.value).street}
 addr:unit	  ???
 addr:city         ${parser.parseLocation(bindings.value).city}
 addr:state        ${parser.parseLocation(bindings.value).state}
 addr:postcode     ${parser.parseLocation(bindings.value).zip}
         `}
-      />
+          />
 
-      <Spacer y={1} />
-      <Button iconRight={<Copy outline="currentColor" />}></Button>
-    </div>
+          <Spacer y={1} />
+          <Button iconRight={<Copy outline="currentColor" />}></Button>
+        </div>
+      </NextUIProvider>
+    </NextThemesProvider>
   );
 }
