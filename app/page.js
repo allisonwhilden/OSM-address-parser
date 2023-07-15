e438e3b226d10a867fc2ac767ce63c55b382b3b3
allisonwhilden <allisonwhilden@gmail.com>
2023-07-15 15:25:13 -0700

    Clean up UI

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
-import Image from "next/image";
-import styles from "./page.module.css";
+import { Textarea, useInput, Button, Spacer } from "@nextui-org/react";
+import { Copy } from "iconoir-react";
 var parser = require("parse-address");
 
 export default function Home() {
@@ -10,94 +10,47 @@ export default function Home() {
   );
   var parsed = parser.parseLocation(address);
 
+  // Controlled
+  const {
+    value: controlledValue,
+    setValue: setControlledValue,
+    reset,
+    bindings,
+  } = useInput("15946 Redmond Way, Suite 103 Redmond, WA 98052");
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-      <label>
-        Address to parse <br />
-        <input
-          name="inputAddress"
-          onKeyDown={(e) =>
-            e.key === "Enter" ? setAddress(e.target.value) : ""
-          }
-        />
-      </label>
-      <div>{parser.parseLocation(address).city}</div>
-      <div className={styles.center}></div>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    >
+      <Textarea
+        {...bindings}
+        width={500}
+        label="Address to parse"
+        minRows={2}
+      />
+      <Spacer y={1} />
+      <Textarea
+        readOnly
+        label="Output"
+        width={500}
+        minRows={6}
+        value={`addr:housenumber  ${parser.parseLocation(bindings.value).number}
+addr:street       ${parser.parseLocation(bindings.value).street}
+addr:unit	  ???
+addr:city         ${parser.parseLocation(bindings.value).city}
+addr:state        ${parser.parseLocation(bindings.value).state}
+addr:postcode     ${parser.parseLocation(bindings.value).zip}
+        `}
+      />
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <Spacer y={1} />
+      <Button iconRight={<Copy outline="currentColor" />}></Button>
+    </div>
   );
 }
